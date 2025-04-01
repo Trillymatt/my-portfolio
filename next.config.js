60d94b7799f767e6751ab0806a0db107825f5ade
Matthew Norman <normanmatthew38@gmail.com>
2025-04-01 10:32:14 -0500

    Changes for deployment

--- next.config.js
+++ next.config.js
@@ -8,25 +8,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Ensure proper handling of static files
-  distDir: '.next',
-  generateEtags: true,
-  // Remove experimental features that might cause issues
-  // experimental: {
-  //   optimizeCss: true,
-  //   optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
-  // },
-  // Remove webpack config as it might cause issues
-  // webpack: (config, { dev, isServer }) => {
-  //   if (!dev && !isServer) {
-  //     config.devtool = 'source-map';
-  //   }
-  //   return config;
-  // },
-  // Remove env config as it's handled by amplify.yml
-  // env: {
-  //   NODE_ENV: process.env.NODE_ENV,
-  // },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
